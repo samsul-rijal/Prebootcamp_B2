139e1c71a3b40772865136e7832c81756198dd12
Samsul Rijal <72549004+samsul-rijal@users.noreply.github.com>
2021-03-25 21:47:21 +0700

    add Reverse Task

---
+++ 2.Reverse/app.js
@@ -0,0 +1,9 @@
+function printAngka() {
+    let input = document.getElementById("inputAngka").value;
+
+    let angka = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        angka = angka + input[i];
+    }
+    document.getElementById("output-angka").innerHTML = "Output : " + angka;
+}
