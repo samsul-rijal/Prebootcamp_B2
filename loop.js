d8546a275e8e101b9b2e9c64fc6a233778e06a0f
samsul-rijal <samsulr99@gmail.com>
2021-03-29 18:35:20 +0700

    add Looping Task

--- loop.js
+++ loop.js
@@ -1,33 +1,27 @@
+
 function printAngka() {
     let input = document.getElementById("inputAngka").value;
-    let angka = "";
+    let temp = [];
     for (i = 1; i <= input; i++) {
-      angka += " " + i;
+        temp.push(i);
     }
-    document.getElementById("output-angka").innerHTML = angka;
+    document.getElementById("output-angka").innerHTML = temp;
 }
-
+    
 function printKuadrat() {
     let input = document.getElementById("inputKuadrat").value;
-    let kuadrat = "";
-    for (a = 1; a <= input; a++){
-        if (Math.sqrt (a) % 1 == 0)
-        kuadrat += " " + a;
+    let temp = [];
+    for (i = 1; i <= input; i++) {
+        temp.push(i * i);
     }
-    document.getElementById("output-kuadrat").innerHTML = kuadrat;
+    document.getElementById("output-kuadrat").innerHTML = temp;
 }
 
 function printKelipatan() {
     let input = document.getElementById("inputKelipatan").value;
-    let kelipatan = "";
-    for (b = 1; b <= 60; b++){
-        if (b % 3 == 0)
-        kelipatan += " " + b;
+    let temp = [];
+    for (i = 1; i <= input; i++) {
+        temp.push(i * 3);
     }
-    document.getElementById("output-kelipatan").innerHTML = kelipatan;
+    document.getElementById("output-kelipatan").innerHTML = temp;
 }
-
-    // for (b = 1; b <= 60; b++){
-    //     if (b % 3 == 0)
-    //     document.write(" " + b);
-    // }
