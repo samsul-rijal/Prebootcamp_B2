e27d97c61144d36a8e8581306d29b55c9e53d86a
samsul-rijal <samsulr99@gmail.com>
2021-03-30 01:25:19 +0700

    add Math Logic Task

--- math_logic.js
+++ math_logic.js
@@ -1,27 +1,36 @@
 function printGaji (){
+    let gajiGolongan, perhari, bulanan, lembur;
+    
     let nama = document.getElementById("inputNama").value;
     let golongan = document.getElementById("inputGolongan").value;
+    let jamBekerja = document.getElementById("inputJam").value;
 
     if (golongan == "A") {
-        gajiGolongan = "1000";
+        gajiGolongan = 1000;
     } else if (golongan == "B") {
-        gajiGolongan = "2000";
+        gajiGolongan = 2000;
     } else if (golongan == "C") {
-        gajiGolongan = "3000";
+        gajiGolongan = 3000;
     } else if (golongan == "D") {
-        gajiGolongan = "4000";
+        gajiGolongan = 4000;
+    } else {
+        alert("Silahkan pilih golongan!")
     }
 
-    let lamaBekerja = document.getElementById("inputJam").value;
-    perhari = gajiGolongan * lamaBekerja;
+    perhari = gajiGolongan * jamBekerja;
     bulanan = perhari * 30;
-    lembur = 48000;
-    totalGaji = bulanan + lembur;
+
+    let totalJam = jamBekerja * 30;
+
+    if (totalJam > 200) {
+        jamLembur = (totalJam - 200);
+        lembur = (jamLembur * (gajiGolongan + 200));
+    }
    
     document.getElementById("output-nama").innerHTML = "Nama: " + nama;
     document.getElementById("output-golongan").innerHTML = "Golongan: " + golongan;
     document.getElementById("output-perhari").innerHTML = "Gaji Perhari: Rp " + perhari;
     document.getElementById("output-bulanan").innerHTML = "Gaji Bulanan: Rp " + bulanan;
     document.getElementById("output-lembur").innerHTML = "Lembur: Rp " + lembur;
-    document.getElementById("output-total").innerHTML = "Total Keseluruhan: Rp " + totalGaji;
+    document.getElementById("output-total").innerHTML = "Total Keseluruhan: Rp " + (bulanan + lembur);
 }
